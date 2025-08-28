90ac799a3386e2a0ce50556cb07d767d9eec21e2
Adrian <adrian.kuhn@arcor.de>
2025-08-28 20:47:07 +0200

    cicd: add dockerfile support

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from asyncio import sleep
+
+
+def main():
+    while True:
+        print("Hello World!")
+        sleep(10)
+
+if __name__ == "__main__":
+    main()
---
+++ build_image.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "$SCRIPT_DIR/project.properties"
+
+docker build -t "$PROJECT_NAME:$VERSION" "$SCRIPT_DIR"
